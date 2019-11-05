05e642247c40daf466b791f2af07f9d9e3ef3d09
Cyrill von Wattenwyl <eni@e23.ch>
2019-11-05 23:43:12 +0100

    implement rtc ds1307

--- src/main.cpp
+++ src/main.cpp
@@ -1,50 +1,63 @@
 #include "Arduino.h"
 #include "config.h"
-//#include <Wire.h>
+#include <Wire.h>
 #include "debouncer.cpp"
 
 #include <Rotary.h>   // platformio lib install 275
-// #include <DS3231.h>   // platformio lib install 1379
+#include <DS1307.h>   // platformio lib install 145
+
 
 
 // time setting & encoder
-Rotary encoder = Rotary(ENCODER_PIN_UP, ENCODER_PIN_DOWN);
+Rotary encoder = Rotary( ENCODER_PIN_UP, ENCODER_PIN_DOWN );
 Debouncer btn_debounce( 390 );
 
+// RTC stuff
+DS1307 rtc;
+uint8_t sec, min, hour, day, month;
+uint16_t year;
+
+// state variables
 bool pulse_direction;
 uint8_t serial_data;
 
+
 // Serial debug macros
 #ifdef ENABLE_SERIAL_DBG
-  #define serial_debug(args...) Serial.print(args);
-  #define serial_debugln(args...) Serial.println(args);
+  #define serial_debug( args... ) Serial.print( args );
+  #define serial_debugln( args... ) Serial.println( args );
 #else
-  #define serial_debug(args);
-  #define serial_debugln(args);
+  #define serial_debug( args );
+  #define serial_debugln( args );
 #endif
 
 
 //
 // Clock / Pulse functions
 //
-void pulse(){
-  if (pulse_direction){
-    serial_debugln("pulse +");
-    digitalWrite(PIN_CLOCK1, HIGH);
-    digitalWrite(PIN_CLOCK2, LOW);
-  }
-  else {
-    serial_debugln("pulse -");
-    digitalWrite(PIN_CLOCK1, LOW);
-    digitalWrite(PIN_CLOCK2, HIGH);
-  }
-  pulse_direction = !pulse_direction;
-  delay(160);
-  digitalWrite(PIN_CLOCK1, LOW);
-  digitalWrite(PIN_CLOCK2, LOW);
+
+
+// update rtc clock
+void zero_rtc() {
+  rtc.set( 0, 0, 0, 5, 11, 2019 );
 }
 
 
+// goto next clock blade
+void pulse() {
+  if ( pulse_direction ){
+    digitalWrite( PIN_CLOCK1, HIGH);
+    digitalWrite(PIN_CLOCK2, LOW);
+  }
+  else {
+    digitalWrite( PIN_CLOCK1, LOW );
+    digitalWrite( PIN_CLOCK2, HIGH );
+  }
+  pulse_direction = !pulse_direction;
+  delay(160);
+  digitalWrite( PIN_CLOCK1, LOW );
+  digitalWrite( PIN_CLOCK2, LOW );
+}
 
 
 //
@@ -52,27 +65,30 @@ void pulse(){
 //
 
 // gets called when encoder button pressed
-void encoder_btn_interrupt(){
+void encoder_btn_interrupt() {
   // check if press is bounce
-  if ( !btn_debounce.check() ){
+  if ( !btn_debounce.check() ) {
     return;
   }
   serial_debugln("btn press");
-
 }
+
 
 // process encoder input
 // gets called every loop run
-void encoder_loop(){
+void encoder_loop() {
 
   // Check if encoder has moved
   unsigned char enc_dir = encoder.process();
 
   // if yes, process encoder movement
-  if ( enc_dir ){
-    if (enc_dir == DIR_CW){
+  if ( enc_dir ) {
+    if ( enc_dir == DIR_CW ){
+      zero_rtc();
       pulse();
-    } else {
+    }
+     else {
+      zero_rtc();
       pulse();
     }
   }
@@ -99,10 +115,10 @@ void setup() {
   );
   sei();
 
-  digitalWrite(PIN_CLOCK1, LOW);
-  digitalWrite(PIN_CLOCK2, LOW);
+  digitalWrite( PIN_CLOCK1, LOW );
+  digitalWrite( PIN_CLOCK2, LOW );
 
-  // init serial & rs485 SoftwareSerial
+  // init serial
   #ifdef ENABLE_SERIAL_DBG
     Serial.begin( SERIAL_BAUD );
   #endif
@@ -113,22 +129,18 @@ void setup() {
   pinMode( LED_BUILTIN_RX, OUTPUT );
   digitalWrite( LED_BUILTIN_RX, HIGH );
 
-
 }
+
 
 // loop function
 void loop() {
 
   // process time setting
   encoder_loop();
-  if (Serial.available()) {
-    serial_data = Serial.read();
-    if (serial_data == 32 || serial_data == 13){
-      pulse();
-    }
-    serial_debugln(serial_data);
-  }
-  //pulse();
 
+  rtc.get( &sec, &min, &hour, &day, &month, &year );
+  if (sec == 0){
+    pulse();
+  }
 
 }
